2f48a30c10c525e7b3bfa4f5a3f7e728c804b63e
Ben London <blondon@cs.umd.edu>
2013-05-31 14:11:08 -0400

    added heuristic seeding; outputs to file at each iteration

--- src/main/java/edu/umd/cs/linqs/apan/APANDataloader.groovy
+++ src/main/java/edu/umd/cs/linqs/apan/APANDataloader.groovy
@@ -6,18 +6,20 @@ import org.slf4j.LoggerFactory
 import edu.umd.cs.psl.config.ConfigBundle
 import edu.umd.cs.psl.config.ConfigManager
 import edu.umd.cs.psl.database.DataStore
-import edu.umd.cs.psl.database.Database;
-import edu.umd.cs.psl.database.DatabasePopulator;
-import edu.umd.cs.psl.database.DatabaseQuery;
+import edu.umd.cs.psl.database.Database
+import edu.umd.cs.psl.database.DatabasePopulator
 import edu.umd.cs.psl.database.Partition
 import edu.umd.cs.psl.database.rdbms.RDBMSDataStore
 import edu.umd.cs.psl.database.rdbms.driver.H2DatabaseDriver
 import edu.umd.cs.psl.database.rdbms.driver.H2DatabaseDriver.Type
 import edu.umd.cs.psl.groovy.PSLModel
 import edu.umd.cs.psl.model.argument.ArgumentType
-import edu.umd.cs.psl.model.argument.GroundTerm;
-import edu.umd.cs.psl.model.argument.Variable;
-import edu.umd.cs.psl.ui.loading.InserterUtils;
+import edu.umd.cs.psl.model.argument.GroundTerm
+import edu.umd.cs.psl.model.argument.UniqueID
+import edu.umd.cs.psl.model.argument.Variable
+import edu.umd.cs.psl.model.atom.GroundAtom
+import edu.umd.cs.psl.ui.loading.InserterUtils
+import edu.umd.cs.psl.util.database.Queries
 
 /** INITIALIZATION **/
 
@@ -37,15 +39,16 @@ PSLModel m = new PSLModel(this, data)
 /** MODEL **/
 
 /* Number of groups */
-int numGroups = cb.getInt('numGroups', -1);
+//int numGroups = cb.getInt('numGroups', -1);
 
 /* Predicates */
 m.add predicate: "Discussion", types: [ArgumentType.UniqueID];
 m.add predicate: "Word", types: [ArgumentType.UniqueID, ArgumentType.String];
+m.add predicate: "Group", types: [ArgumentType.UniqueID, ArgumentType.String];
 m.add predicate: "UsedWord", types: [ArgumentType.UniqueID, ArgumentType.UniqueID];
 m.add predicate: "Friends", types: [ArgumentType.UniqueID, ArgumentType.UniqueID];
 m.add predicate: "PostsIn", types: [ArgumentType.UniqueID, ArgumentType.UniqueID];
-m.add predicate: "InGroup", types: [ArgumentType.UniqueID, ArgumentType.Integer];
+m.add predicate: "InGroup", types: [ArgumentType.UniqueID, ArgumentType.UniqueID];
 
 
 /** LOAD DATA **/
@@ -70,6 +73,11 @@ inserter = data.getInserter(Word, eviPart);
 InserterUtils.loadDelimitedData(inserter, dataDir + "Words.csv", ",");
 System.out.println("  Loaded Word predicate");
 
+/* Groups */
+inserter = data.getInserter(Group, eviPart);
+InserterUtils.loadDelimitedData(inserter, dataDir + "Groups.csv", ",");
+System.out.println("  Loaded Group predicate");
+
 /* Friends */
 inserter = data.getInserter(Friends, eviPart);
 InserterUtils.loadDelimitedData(inserter, dataDir + "Friends.csv", ",");
@@ -88,45 +96,75 @@ System.out.println("  Loaded PostsIn predicate");
 
 /** POPULATE E-/M-step partitions **/
 
+Database db;
+Set<GroundAtom> atoms;
+DatabasePopulator dbPop;
+
 System.out.println("Populating E-/M-step paritions ...");
 
-///* Builds sets of user IDs and target interactions */
-//Set<GroundTerm> users = new HashSet<GroundTerm>();
-//Set<GroundTerm> targetInteractions = new HashSet<GroundTerm>();
-//
-//Database db = data.getDatabase(targetInteractionsPart);
-//DatabaseQuery query = new DatabaseQuery((TargetInteraction(UserA, UserB)).getFormula());
-//results = db.executeQuery(query);
-//for (int iResults = 0; iResults < results.size(); iResults++) {
-//    users.add(results.get(iResults, UserA.toAtomVariable()));
-//    targetInteractions.add(results.get(iResults, UserB.toAtomVariable()));
+/* Build substitution map */
+Map<Variable, Set<GroundTerm>> submap = new HashMap<Variable, Set<GroundTerm>>();
+Variable U = new Variable("U");
+Variable G = new Variable("G");
+Variable T = new Variable("T");
+Set<GroundTerm> users = new HashSet<GroundTerm>();
+Set<GroundTerm> groups = new HashSet<GroundTerm>();
+Set<GroundTerm> threads = new HashSet<GroundTerm>();
+submap.put(U, users);
+submap.put(G, groups);
+submap.put(T, threads);
+
+/* Open evidence partition */
+db = data.getDatabase(eviPart);
+
+/* Add all users from Friends predicate (should contain all users) */
+atoms = Queries.getAllAtoms(db, Friends);
+for (GroundAtom a : atoms) {
+	users.add(a.getArguments()[0]);
+}
+atoms = Queries.getAllAtoms(db, UsedWord);
+for (GroundAtom a : atoms) {
+	users.add(a.getArguments()[0]);
+}
+System.out.println(String.format("  Added %d users", users.size()));
+
+/* Add all groups */
+//for (int g = 0; g < numGroups; g++) {
+//	UniqueID groupID = data.getUniqueID(g);
+//	groups.add(groupID);
 //}
-//
-//System.out.println("Num users: " + users.size());
-//System.out.println("Target user interactions: " + targetInteractions.size());
-//db.close()
-//
-///* Builds set of cluster terms */
-//Set<GroundTerm> clusters = new HashSet<GroundTerm>();
-//for (int iCluster = 0; iCluster < numClusters; iCluster++)
-//    clusters.add(data.getUniqueID(iCluster));
-//    
-///* Performs population */
-//Map<Variable, Set<GroundTerm>> submap = new HashMap<Variable, Set<GroundTerm>>();
-//submap.put(new Variable("U"), users);
-//submap.put(new Variable("C"), clusters);
-//submap.put(new Variable("TI"), targetInteractions);
-//
-//db = data.getDatabase(eStepClusterPart);
-//DatabasePopulator populator = new DatabasePopulator(db);
-//populator.populate(InCluster(U, C).getFormula(), submap);
-//db.close();
-//System.out.println("Populated E step cluster atoms");
-//
-//db = data.getDatabase(mStepPart);
-//populator = new DatabasePopulator(db);
-//populator.populate(InCluster(U, C).getFormula(), submap);
-//populator.populate(TargetInteraction(U, TI).getFormula(), submap);
-//db.close();
-//System.out.println("Populated M step cluster and TargetInteraction atoms");
+atoms = Queries.getAllAtoms(db, Group);
+for (GroundAtom a : atoms) {
+	groups.add(a.getArguments()[0]);
+}
+System.out.println(String.format("  Added %d groups", groups.size()));
+
+/* Close evidence partition */
+db.close();
+
+/* Add all threads */
+db = data.getDatabase(tgtPart);
+atoms = Queries.getAllAtoms(db, PostsIn);
+for (GroundAtom a : atoms) {
+	threads.add(a.getArguments()[1]);
+}
+db.close();
+System.out.println(String.format("  Added %d threads", threads.size()));
+
+/* Populate E-step variables */
+db = data.getDatabase(eStepPart);
+dbPop = new DatabasePopulator(db);
+dbPop.populate(InGroup(U,G).getFormula(), submap);
+db.close();
+System.out.println("  Populated E-step variables");
+
+/* Populate M-step variables */
+db = data.getDatabase(mStepPart);
+dbPop = new DatabasePopulator(db);
+dbPop.populate(InGroup(U,G).getFormula(), submap);
+dbPop.populate(PostsIn(U,T).getFormula(), submap);
+db.close();
+System.out.println("  Populated M-step variables");
+
+System.out.println("Finished data loading.")
 
--- src/main/java/edu/umd/cs/linqs/apan/APANExperiment.groovy
+++ src/main/java/edu/umd/cs/linqs/apan/APANExperiment.groovy
@@ -3,34 +3,30 @@ package edu.umd.cs.linqs.apan;
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-import edu.umd.cs.psl.application.inference.MPEInference;
-import edu.umd.cs.psl.application.learning.weight.WeightLearningApplication;
-import edu.umd.cs.psl.application.learning.weight.maxlikelihood.MaxLikelihoodMPE;
-import edu.umd.cs.psl.application.learning.weight.maxlikelihood.MaxPseudoLikelihood;
+import edu.umd.cs.psl.application.inference.MPEInference
+import edu.umd.cs.psl.application.learning.weight.WeightLearningApplication
+import edu.umd.cs.psl.application.learning.weight.maxlikelihood.MaxLikelihoodMPE
 import edu.umd.cs.psl.config.ConfigBundle
 import edu.umd.cs.psl.config.ConfigManager
 import edu.umd.cs.psl.database.DataStore
-import edu.umd.cs.psl.database.Database;
-import edu.umd.cs.psl.database.DatabasePopulator;
-import edu.umd.cs.psl.database.DatabaseQuery;
+import edu.umd.cs.psl.database.Database
 import edu.umd.cs.psl.database.Partition
-import edu.umd.cs.psl.database.ResultList;
 import edu.umd.cs.psl.database.rdbms.RDBMSDataStore
 import edu.umd.cs.psl.database.rdbms.driver.H2DatabaseDriver
 import edu.umd.cs.psl.database.rdbms.driver.H2DatabaseDriver.Type
 import edu.umd.cs.psl.groovy.PSLModel
-import edu.umd.cs.psl.groovy.PredicateConstraint;
-import edu.umd.cs.psl.model.argument.ArgumentType
-import edu.umd.cs.psl.model.argument.GroundTerm;
+import edu.umd.cs.psl.groovy.PredicateConstraint
+import edu.umd.cs.psl.model.argument.GroundTerm
 import edu.umd.cs.psl.model.argument.UniqueID
-import edu.umd.cs.psl.model.argument.Variable;
 import edu.umd.cs.psl.model.atom.GroundAtom
-import edu.umd.cs.psl.model.atom.RandomVariableAtom;
-import edu.umd.cs.psl.model.kernel.Kernel;
-import edu.umd.cs.psl.model.kernel.predicateconstraint.DomainRangeConstraintKernel;
-import edu.umd.cs.psl.model.kernel.predicateconstraint.DomainRangeConstraintType;
-import edu.umd.cs.psl.ui.loading.InserterUtils;
-import edu.umd.cs.psl.util.database.Queries;
+import edu.umd.cs.psl.model.kernel.Kernel
+import edu.umd.cs.psl.util.database.Queries
+
+
+/** DATA LOADER **/
+
+evaluate(new File("src/main/java/edu/umd/cs/linqs/apan/APANDataloader.groovy"));
+
 
 /** INITIALIZATION **/
 
@@ -44,31 +40,36 @@ def defaultPath = System.getProperty("java.io.tmpdir")
 String dbpath = cb.getString("dbpath", defaultPath + File.separator + "apan")
 DataStore data = new RDBMSDataStore(new H2DatabaseDriver(Type.Disk, dbpath, false), cb)
 
-PSLModel m = new PSLModel(this, data)
+PSLModel m = new PSLModel(this, data);
 
 
 /** PARTITIONS **/
 
 Partition eviPart = new Partition(cb.getInt("eviPart", -1));
 Partition tgtPart = new Partition(cb.getInt("tgtPart", -1));
-Partition eStepPart = new Partition(cb.getInt('eStepPart', -1));
-Partition mStepPart = new Partition(cb.getInt('mStepPart', -1));
+Partition eStepPart = new Partition(cb.getInt("eStepPart", -1));
+Partition mStepPart = new Partition(cb.getInt("mStepPart", -1));
 
 
 /** MODEL PARAMETERS **/
 
-int numGroups = cb.getInt('numGroups', -1);
-boolean sq = cb.getBoolean('squared', true);
-
+//int numGroups = cb.getInt("numGroups", -1);
+boolean sq = cb.getBoolean("squared", true);
 
 /* Initialize weights using random Gaussian values */
 Random rand = new Random(311311);
 Map<Kernel,Double> initWeights = new HashMap<Kernel,Double>();
-double variance = cb.getDouble('weightVariance', 1.0);
-double meanWordWeight = cb.getDouble('meanWordWeight', -1.0);
-double meanFriendWeight = cb.getDouble('meanFriendWeight', -1.0);
-double meanGroupWeight = cb.getDouble('meanGroupWeight', -1.0);
-double meanPriorWeight = cb.getDouble('meanPriorWeight', -1.0);
+double variance = cb.getDouble("weightVariance", 1.0);
+double meanWordWeight = cb.getDouble("meanWordWeight", 1.0);
+double meanFriendWeight = cb.getDouble("meanFriendWeight", 1.0);
+double meanGroupWeight = cb.getDouble("meanGroupWeight", 1.0);
+double meanPriorWeight = cb.getDouble("meanPriorWeight", 1.0);
+double seedWeightMult = cb.getDouble("seedWeightMult", 10.0);
+
+/* Predicate groups */
+def evidencePreds = [Discussion, Word, Friends, UsedWord] as Set;
+def labelPreds = [InGroup, PostsIn] as Set;
+
 
 /** RULES **/
 
@@ -86,11 +87,22 @@ initWeights.put(k, initWeight);
 
 /* Relates word usage to group membership */
 Set<GroundAtom> words = Queries.getAllAtoms(eviDB, Word);
+Set<GroundAtom> groups = Queries.getAllAtoms(eviDB, Group);
 for (GroundAtom w : words) {
 	UniqueID wordID = (UniqueID) w.getArguments()[0];
-	for (int g = 0; g < numGroups; g++) {
+	for (GroundAtom g : groups) {
+		UniqueID groupID = (UniqueID) g.getArguments()[0];
 		initWeight = Math.max(meanWordWeight + rand.nextGaussian() * variance, 0.05);
-		k = m.add rule: UsedWord(U,wordID) >> InGroup(U,g+1), weight: initWeight, squared: sq;
+		/* Apply heuristic weighting */
+		if (
+			   (wordID == data.getUniqueID(7) && groupID == data.getUniqueID(1))
+			|| (wordID == data.getUniqueID(2) && groupID == data.getUniqueID(2))
+			|| (wordID == data.getUniqueID(8) && groupID == data.getUniqueID(3))
+			|| (wordID == data.getUniqueID(25) && groupID == data.getUniqueID(4))
+			) {
+			initWeight *= seedWeightMult;
+		}
+		k = m.add rule: UsedWord(U,wordID) >> InGroup(U,groupID), weight: initWeight, squared: sq;
 		initWeights.putAt(k, initWeight);
 	}
 }
@@ -99,16 +111,19 @@ for (GroundAtom w : words) {
 Set<GroundAtom> threads = Queries.getAllAtoms(eviDB, Discussion);
 for (GroundAtom t : threads) {
 	UniqueID threadID = (UniqueID) t.getArguments()[0];
-	for (int g = 0; g < numGroups; g++) {
+	for (GroundAtom g : groups) {
+		UniqueID groupID = (UniqueID) g.getArguments()[0];
 		initWeight = Math.max(meanGroupWeight + rand.nextGaussian() * variance, 0.05);
-		k = m.add rule: InGroup(U,g+1) >> PostsIn(U,threadID), weight: initWeight, squared: sq;
+		k = m.add rule: InGroup(U,groupID) >> PostsIn(U,threadID), weight: initWeight, squared: sq;
 		initWeights.put(k, initWeight);
 	}
 }
 
 /* Priors */
-for (int g = 0; g < numGroups; g++)
-	m.add rule: ~InGroup(U,g+1), weight: meanPriorWeight, squared: sq;
+for (GroundAtom g : groups) {
+	UniqueID groupID = (UniqueID) g.getArguments()[0];
+	m.add rule: ~InGroup(U,groupID), weight: meanPriorWeight, squared: sq;
+}
 	
 /* Constraints */
 m.add PredicateConstraint.Functional, on: InGroup;
@@ -117,40 +132,82 @@ m.add PredicateConstraint.Functional, on: InGroup;
 eviDB.close();
 
 /* Print model */
-//System.out.println(m.toString());
+System.out.println(m.toString());
 
 
 /** EXPERIMENT **/
 
 /* Experiment parameters */
-def numIters = cb.getInt('numIters', -1);
+def numIters = cb.getInt("numIters", 10);
 
-///* Performs EM */
-//eStepToClose = [UsedHashTag, EvidenceInteraction, TargetInteraction] as Set
-//mStepToClose =  [UsedHashTag, EvidenceInteraction] as Set
-//for (int i = 0; i < numIters; i++) {
-//    /* E step: infers cluster membership */
-//    db = data.getDatabase(eStepClusterPart, eStepToClose, backgroundPart, targetInteractionsPart);
-//    MPEInference mpe = new MPEInference(m, db, cb);
-//    mpe.mpeInference();
-//    mpe.close();
-//    db.close();
-//    
-//    /* M step: optimizes parameters */
-//    rvDB = data.getDatabase(mStepPart, mStepToClose, backgroundPart);
-//    obsvDB = data.getDatabase(eStepClusterPart, [InCluster, TargetInteraction] as Set, targetInteractionsPart);
-//    WeightLearningApplication wl = new MaxLikelihoodMPE(m, rvDB, obsvDB, cb);
-////    WeightLearningApplication wl = new MaxPseudoLikelihood(m, rvDB, obsvDB, cb);
-//    wl.learn();
-//    wl.close();
-//    rvDB.close();
-//    obsvDB.close();
-//    
-//}
-//
-//System.out.println("Completed clustering.");
-////System.out.println(m);
-//for (Kernel kernel : m.getKernels())
-//    System.out.println(kernel.toString() + " (Initial: " + initialWeights.get(kernel).toString() + ")");
+/* Performs EM */
+for (int i = 0; i < numIters; i++) {
+	System.out.println("Starting iteration " + i);
+	
+	/* E step: infers cluster membership */
+	Database db = data.getDatabase(eStepPart, evidencePreds + PostsIn, eviPart, tgtPart);
+	MPEInference mpe = new MPEInference(m, db, cb);
+	mpe.mpeInference();
+	mpe.close();
+	//db.close();
+	
+	/* Output memberships to file */
+	Set<GroundAtom> groupMemberships = Queries.getAllAtoms(db, InGroup);
+	File file = new File("output/InGroup.csv");
+	file.createNewFile();
+	FileWriter fw = new FileWriter(file);
+	for (GroundAtom a : groupMemberships) {
+		GroundTerm[] terms = a.getArguments();
+		UniqueID u = (UniqueID) terms[0];
+		UniqueID g = (UniqueID) terms[1];
+		double v = a.getValue();
+		fw.write(String.format("%s,%s,%f\n", u.toString(), g.toString(), v));
+	}
+	fw.flush();
+	fw.close();
+	db.close();
+	
+	/* M step: optimizes parameters */
+	Database rvDB = data.getDatabase(mStepPart, evidencePreds, eviPart);
+	Database obsvDB = data.getDatabase(eStepPart, labelPreds, tgtPart);
+	WeightLearningApplication wl = new MaxLikelihoodMPE(m, rvDB, obsvDB, cb);
+	wl.learn();
+	wl.close();
+	rvDB.close();
+	obsvDB.close();
+}
+
+System.out.println("Completed clustering.");
+System.out.println(m);
+
+
+/** GROUP MEMBERSHIPS **/
+
+/* Infer group memberships */
+System.out.println("Inferring group memberships ...");
+Database db = data.getDatabase(eStepPart, evidencePreds + PostsIn, eviPart, tgtPart);
+MPEInference mpe = new MPEInference(m, db, cb);
+mpe.mpeInference();
+mpe.close();
+
+/* Output memberships to file */
+System.out.println("Writing group memberships to file ...");
+Set<GroundAtom> groupMemberships = Queries.getAllAtoms(db, InGroup);
+File file = new File("output/InGroup.csv");
+file.createNewFile();
+FileWriter fw = new FileWriter(file);
+for (GroundAtom a : groupMemberships) {
+	GroundTerm[] terms = a.getArguments();
+	UniqueID u = (UniqueID) terms[0];
+	UniqueID g = (UniqueID) terms[1];
+	double v = a.getValue();
+	fw.write(String.format("%s,%s,%f\n", u.toString(), g.toString(), v));
+}
+fw.flush();
+fw.close();
+db.close();
+
+System.out.println("Done!");
+
 
 
